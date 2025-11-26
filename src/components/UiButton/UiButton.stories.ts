9660b41cc300d3794f4183327afebb4dc95a6c44
kissme676 <Varvaraudalaa666@gmail.com>
2025-11-26 18:53:47 +0500

    внесены изменения в в кнопку и название пакета

---
+++ src/components/UiButton/UiButton.stories.ts
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/vue3';
+
+import { UiButton } from '..';
+import { html } from '../../helpers';
+
+const meta: Meta<typeof UiButton> = {
+  component: UiButton,
+  args: {},
+  argTypes: {
+    layout: {
+      options: ['primary', 'secondary'],
+    },
+    size: {
+      options: ['m', 'l'],
+    },
+  },
+};
+
+export default meta;
+
+export const Primary: StoryObj<typeof UiButton> = {
+  render: (args) => ({
+    components: { UiButton },
+    setup: () => ({ args }),
+
+    template: html` <UiButton v-bind="args">Текст</UiButton>`,
+  }),
+};
---
+++ src/components/UiButton/UiButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <button :class="$style.button" :data-layout="props.layout" :data-size="props.size" :disabled="props.isDisabled" :type="props.type">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  layout?: 'primary' | 'secondary';
+  type?: 'submit' | 'button';
+  isDisabled?: boolean;
+  size?: 'm' | 'l';
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+  layout: 'primary',
+  type: 'button',
+  size: 'm'
+});
+</script>
+
+<style module lang="scss">
+.button {
+  position: relative;
+  display: inline-flex;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  padding: 16px 32px;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: var(--color-white);
+  background: var(--color-primary);
+  border-radius: 16px;
+
+  &[data-size='l'] {
+    height: 48px;
+    padding: 20px 40px;
+    font-size: 1.125rem;
+  }
+
+  &[data-layout='secondary'] {
+    color: var(--color-primary);
+    background-color: var(--color-transparent);
+    border-color: var(--color-primary);
+  }
+}
+</style>
